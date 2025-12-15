9755a014c8fabd10a5c0de907979a093b04c7a4f
DemLinkx <dismas007135@gmail.com>
2025-12-15 20:10:36 +0300

    Update func.h

--- func.h
+++ func.h
@@ -13,6 +13,15 @@ struct expn_pars
 double expn(double x, const void *par);
 
 
+/*
+ * degree — степень полинома
+ * coeff  — массив коэффициентов длины degree + 1
+ * Коэффициенты хранятся в следующем порядке:
+ * coeff[0] — коэффициент при x^0 
+ * ...
+ *coeff[degree] — коэффициент при x^degree
+ */
+
 struct pol_pars
 {
     size_t degree;
@@ -24,11 +33,29 @@ struct pol_pars
 double pol(double x, const void *par);
 
 
-// моя дополнительная
+/*
+ * Функция sin(x).
+ * Эта функция не имеет дополнительных параметров.
+ * Принимает:
+ *   - x   - точка вычисления
+ *   - par — не используется 
+ * Возвращает:
+ *   - sin(x)
+ */
+
 double fsin(double x, const void *par);
 
 
-// моя дополнительная
+/*
+ * Функция cos(x).
+ * Эта функция не имеет дополнительных параметров.
+ * Принимает:
+ *   - x   — точка вычисления
+ *   - par — не используется (должен быть NULL)
+ * Возвращает:
+ *   - cos(x)
+ */
+
 double fcos(double x, const void *par);
 
 #endif
