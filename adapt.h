ab6a451ea386aaaf6bfd659a10941a87601456ca
DemLinkx <dismas007135@gmail.com>
2025-12-15 20:08:38 +0300

    Update adapt.h

--- adapt.h
+++ adapt.h
@@ -2,6 +2,10 @@
 #define adaptato
 #include "quad.h"
 
+typedef double (*fnc_t)(double x, const void *par);
+
+typedef double (*quad_t)(fnc_t f, const void *par,
+                         double a, double b, size_t n);
 
 double quad_adapt(fnc_t f, const void *par,
                   double a, double b, double eps,
