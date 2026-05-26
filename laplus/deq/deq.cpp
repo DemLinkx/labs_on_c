0e0711b97a31710604a462d7cb414874959bbcbe
Dmitrij <dmitrij@MacBook-Pro-2.local>
2026-05-27 02:18:55 +0300

    fix: replace deq to CircList

--- laplus/deq/deq.cpp
+++ laplus/deq/deq.cpp
@@ -1,210 +1,177 @@
 #include <iostream>
-
-// Deq: Head (front) - - - Tail (back)
-// Node: [<-pred -- data -- next->]
-
 template<typename T>
-class Deq;
+class CircularList;
 
 template<typename T>
 class Node{
-    friend class Deq<T>; 
-    public:
-        Node(const T& data, Node* pred = NULL, Node* next = NULL); 
-            // ии сказал что конструктора по умолчанию 
-            // не хватит и надо свой писать,
-            // но я не совсем понимаю почему, типо это просто дурной тон, или прям ошибка?
-            // но при этом если осатвить конструктор по умолчанию, 
-            // то все скомпилируется нормально
-    private:
-        T data = T(); // типо для int будет 0, для double 0.0, для string = '' итд
-        Node* pred = NULL;
-        Node* next = NULL;
-        
-};
-
-
-template<typename T>
-Node<T>::Node(const T& data, Node* pred, Node* next){ 
-
-    // хотелось два раза указать значения по умолчанию, 
-    // ибо сигнатуры должны совпадать, но по идее если разрешить параметры по умолчанию и там и там,
-    // то может быть двойное определение и будет плохо
-
-    this->data = data;
-    this->pred = pred;
-    this->next = next;
-}
-
-template<typename T>
-class Deq{
-    public:
-        Deq();
-        Deq(Deq<T>&& other) noexcept; // обязательно ли писать noexcept 
-        Deq(const Deq<T>& other);  // что будет если я не сделаю ссылку константной? 
-        Deq<T>& operator=(const Deq<T>& other);
-        Deq<T>& operator=(Deq<T>&& other); // возвращаем всегда Lvalue 
-        ~Deq() noexcept; // без noexcept типо медленнее ?
-
-        void push_back(T data);
-        void push_front(T data);
-        void pop_back();
-        void pop_front();
-        void clean() noexcept;
-        size_t get_len() const {return this->len;};
-        bool is_empty() const {return !bool(this->len);};
-
-    private:
-        size_t len = 0;
-        Node<T>* head = NULL; // Нужно ли заменить на nullptr? Типо это более безопасно?
-        Node<T>* tail = NULL;
+friend class CircularList<T>;
+public:
+Node(const T& data, Node* pred = NULL, Node* next = NULL);
+private:
+T data = T();
+Node* pred = NULL;
+Node* next = NULL;
 };
 
 template<typename T>
-Deq<T>& Deq<T>::operator=(Deq<T>&& other){ // Lvalue(Rvalue)
-    if (this != &other){
-        this->clean(); // ???
-        this->len = other.len;
-        this->head = other.head;
-        this->tail = other.tail;
-        other.len = 0;
-        other.head = NULL;
-        other.tail = NULL;
-    }
-    return *this;
+Node<T>::Node(const T& data, Node* pred, Node* next){
+this->data = data;
+this->pred = pred;
+this->next = next;
 }
 
 template<typename T>
-Deq<T>& Deq<T>::operator=(const Deq<T>& other){  // Lvalue(Lvalue)
-    if(this != &other){
-        this->clean();
-        auto start = other.head; // Node<T>*
-        for (int i = 0; i < other.len; i++){
-            this->push_back(start->data);
-            start = start->next; 
-        }
-    }
-    return *this;
+class CircularList{
+public:
+CircularList();
+CircularList(CircularList<T>&& other) noexcept;
+CircularList(const CircularList<T>& other);
+CircularList<T>& operator=(const CircularList<T>& other);
+CircularList<T>& operator=(CircularList<T>&& other);
+~CircularList() noexcept;
+void insert_after(T data);
+void insert_before(T data);
+void remove_current();
+void move_next();
+void move_prev();
+void clean() noexcept;
+size_t get_len() const {return this->len;}
+bool is_empty() const {return !bool(this->len);}
+T& current() {return this->now->data;}
+private:
+size_t len = 0;
+Node<T>* now = NULL;
+};
+
+template<typename T>
+CircularList<T>& CircularList<T>::operator=(CircularList<T>&& other){
+if (this != &other){
+this->clean();
+this->len = other.len;
+this->now = other.now;
+other.len = 0;
+other.now = NULL;
+}
+return *this;
 }
 
 template<typename T>
-Deq<T>::Deq(Deq<T>&& other) noexcept{ // лутаемся
-    this->len = other.len;
-    this->head = other.head;
-    this->tail = other.tail;
-    other.len = 0;
-    other.head = NULL;
-    other.tail = NULL;
+CircularList<T>& CircularList<T>::operator=(const CircularList<T>& other){
+if(this != &other){
+this->clean();
+if (other.len == 0) return *this;
+auto start = other.now;
+for (size_t i = 0; i < other.len; i++){
+this->insert_after(start->data);
+start = start->next;
+}
+}
+return *this;
 }
 
 template<typename T>
-Deq<T>::Deq(const Deq<T>& other){
-    this->len = 0;
-    this->head = NULL;
-    this->tail = NULL;
-    auto start = other.head; // Node<T>*
-        for (int i = 0; i < other.len; i++){
-            this->push_back(start->data);
-            start = start->next; 
-    }
-}
-
-
-template<typename T>
-void Deq<T>::push_back(T data){
-    Node<T>* nd = new Node(data, this->tail, NULL); // вроде не нужно проверять bad alloc?
-
-    if(this->len == 0){
-        this->tail = nd;
-        this->head = nd;
-        this->len++;
-    }
-    else{
-        this->tail->next = nd;
-        this->tail = nd;
-        this->len++;
-    }
+CircularList<T>::CircularList(CircularList<T>&& other) noexcept{
+this->len = other.len;
+this->now = other.now;
+other.len = 0;
+other.now = NULL;
 }
 
 template<typename T>
-void Deq<T>::push_front(T data){
-    Node<T>* nd = new Node(data, NULL, this->head);
-    
-    if (this->len == 0){
-        this->head = nd;
-        this->tail = nd;
-        this->len++;
-    } else{
-        this->head->pred = nd;
-        this->head = nd;
-        this->len++;
-    }
+CircularList<T>::CircularList(const CircularList<T>& other){
+this->len = 0;
+this->now = NULL;
+if (other.len == 0) return;
+auto start = other.now;
+for (size_t i = 0; i < other.len; i++){
+this->insert_after(start->data);
+start = start->next;
+}
 }
 
 template<typename T>
-void Deq<T>::pop_front(){
-    if (this->len == 0){
-        std::cout<<"list is empty";
-        return;
-    }else{
-        Node<T>* dltnd = this->head;
-        this->len--; // чтобы не писать два раза -- для последнего узла и для какого-то
+void CircularList<T>::insert_after(T data){
+if (this->len == 0){
+Node<T>* nd = new Node(data, NULL, NULL);
+nd->pred = nd;
+nd->next = nd;
+this->now = nd;
+this->len = 1;
+} else {
+Node<T>* nd = new Node(data, this->now, this->now->next);
+nd->next->pred = nd;
+this->now->next = nd;
+this->now = nd;
+this->len++;
+}
+}
 
-        if (this->len == 0){
-            this->head = NULL;
-            this->tail = NULL;
-            delete dltnd;
+template<typename T>
+void CircularList<T>::insert_before(T data){
+if (this->len == 0){
+Node<T>* nd = new Node(data, NULL, NULL);
+nd->pred = nd;
+nd->next = nd;
+this->now = nd;
+this->len = 1;
+} else {
+Node<T>* nd = new Node(data, this->now->pred, this->now);
+nd->pred->next = nd;
+this->now->pred = nd;
+this->now = nd;
+this->len++;
+}
+}
 
-        }else{
-            this->head = dltnd->next;
-            dltnd->next->pred = NULL;
-            delete dltnd;
+template<typename T>
+void CircularList<T>::remove_current(){
+if (this->len == 0){
+std::cout << "list is empty";
+return;
+}
+Node<T>* dltnd = this->now;
+if (this->len == 1){
+this->now = NULL;
+this->len = 0;
+delete dltnd;
+} else {
+dltnd->pred->next = dltnd->next;
+dltnd->next->pred = dltnd->pred;
+this->now = dltnd->next;
+this->len--;
+delete dltnd;
+}
+}
 
-        }
-    }
+template<typename T>
+void CircularList<T>::move_next(){
+if (this->len > 0){
+this->now = this->now->next;
+}
+}
+
+template<typename T>
+void CircularList<T>::move_prev(){
+if (this->len > 0){
+this->now = this->now->pred;
+}
+}
+
+template<typename T>
+void CircularList<T>::clean() noexcept{
+if (this->len == 0){
+return;
+}
+while (this->len != 0){
+this->remove_current();
+}
+}
+
+template<typename T>
+CircularList<T>::~CircularList() noexcept{
+this->clean();
+}
+
+int main(){
     ;
-}
-
-//(head)[NULL, ...,  next->] - [<-pred, ..., next->] - ... - [<-pred, ..., next->] - [<-pred, ..., NULL](tail)
-
-template<typename T>
-void Deq<T>::pop_back(){
-
-    if (this->len == 0){
-        std::cout<<"list is empty";
-        return;
-    }else{
-        Node<T>* dltnd = this->tail;
-        this->len--; // чтобы не писать два раза -- для последнего узла и для какого-то
-
-        if (this->len == 0){
-            this->head = NULL;
-            this->tail = NULL;
-            delete dltnd;
-
-        }else{
-            this->tail = dltnd->pred;
-            dltnd->pred->next = NULL;
-
-            delete dltnd;
-        }
-    }
-    ;
-}
-
-template<typename T>
-void Deq<T>::clean() noexcept{
-    if (this->len == 0){
-        std::cout<<"list is empty";
-        return;
-    }
-    while (this->len != 0){
-        this->pop_back();
-    }
-}
-
-
-template<typename T>
-Deq<T>::~Deq() noexcept{
-    this->clean();
 }
